2f8fb5e6470eefad34b73f257d6fd8eb6f1b38ce
Cristiano Amorim <camorimpe@gmail.com>
2026-09-10 14:05:37 -0300

    Modify background gradient opacity
    Updated background gradient opacity in app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.markdown(
     <style>
       .stApp {{
         background-image:
-            linear-gradient(rgba(240, 244, 248, 0.60), rgba(240, 244, 248, 0.60)),
+            linear-gradient(rgba(240, 244, 248, 0.80), rgba(240, 244, 248, 0.80)),
             url("{url_imagem_fundo}");
         background-size: cover;
         background-position: center;
